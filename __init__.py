8d4cdd4c9e6f0b0d9784dd4da4b06d1044d2fd26
GaboMeister <ggluamizt@gmail.com>
2025-10-22 22:10:13 -0600

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 21 01:55:42 2025
+
+@author: Gabo San
+"""
+
+# Deja esto vacío o con un breve comentario.
+# Convierte a 'backend' en un paquete importable.
